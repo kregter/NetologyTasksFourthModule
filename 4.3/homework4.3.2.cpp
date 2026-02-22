5bec0a56c786eec8f9a921155e74316a3d051a4a
kregter <pihteevn@ru-drive.com>
2026-02-22 15:16:16 +0300

    Add files via upload

---
+++ 4.3/homework4.3.2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <vector>
+
+template <class T>
+class Table {
+    int rows, cols;
+    T* data;
+public:
+    Table(int r, int c) : rows(r), cols(c) {
+        data = new int[rows * cols] {};
+    }
+
+    ~Table() {
+        delete[] data;
+    }
+
+    int* operator[](int row) {
+        return data + row * cols;
+    }
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+    auto test = Table<int>(2, 2);
+    test[0][0] = 4;
+
+    std::cout << test[0][0];
+
+	return 0;
+
+}
+
+
