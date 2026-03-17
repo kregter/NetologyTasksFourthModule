5c325013d944510287691f0db328964cca3ea029
kregter <pihteevn@ru-drive.com>
2026-03-17 23:02:14 +0300

    Add files via upload

---
+++ 4.5/homework4.5.1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	std::vector<int> numbers {1, 1, 2, 5, 6, 1, 2, 4};
+
+	std::cout << "[IN]: ";
+	for (auto it = numbers.begin(); it != numbers.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	std::sort(numbers.begin(), numbers.end());
+
+	auto it = std::unique(numbers.begin(), numbers.end());
+
+	numbers.erase(it, numbers.end());
+
+	std::cout << "[OUT]: ";
+	for (auto it = numbers.begin(); it != numbers.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
