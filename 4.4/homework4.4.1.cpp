ed6fbd93e2db51e419519d753fbf28016adc5b3d
kregter <pihteevn@ru-drive.com>
2026-03-17 20:11:30 +0300

    Add files via upload

---
+++ 4.4/homework4.4.1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+	std::string input;
+	std::cout << "[IN]: ";
+	std::getline(std::cin, input);
+
+	std::map<char, int> letters{};
+
+	for (const auto& ch : input) {
+		auto it = letters.find(ch);
+		if (it != letters.end()) {
+			it->second++;
+		}
+		else {
+			letters[ch] = 1;
+		}
+	}
+
+	std::multimap<int, char> letters_sorted{};
+
+	for (const auto& let : letters) {
+		letters_sorted.insert({ let.second, let.first });
+	}
+	
+	std::cout << "[OUT]: " << std::endl;
+
+	for (auto let = letters_sorted.rbegin(); let != letters_sorted.rend(); ++let) {
+		std::cout << let->second << ": " << let->first << std::endl;
+	}
+
+
+	return 0;
+
+}
