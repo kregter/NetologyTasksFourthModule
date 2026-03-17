27e32954f2aad0aa5b4814a8a2b93d62a58245b8
kregter <pihteevn@ru-drive.com>
2026-03-17 20:12:02 +0300

    Add files via upload

---
+++ 4.4/homework4.4.2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int num = 0;
+	std::cout << "[IN]: " << std::endl;
+	std::cin >> num;
+
+	std::set<int> numbers_set{};
+	int num_from_console = 0;
+
+	for (int i = 0; i < num; i++) {
+		std::cin >> num_from_console;
+		numbers_set.insert(num_from_console);
+	}
+
+	std::cout << "[OUT]: " << std::endl;
+
+	for (auto num = numbers_set.rbegin(); num != numbers_set.rend(); ++num) {
+		std::cout << *num << std::endl;
+	}
+
+	return 0;
+}
